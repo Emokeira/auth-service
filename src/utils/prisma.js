006a86a56e550dba96379adb9c01eab6199ed9ee
Edith Mokeira <edithmokeira7@gmail.com>
2025-05-19 23:32:01 +0300

    Added backend and frontend code for authentication service

--- src/utils/prisma.js
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require ('@prisma/client');
-const prisma = new PrismaClient();
-
-module.exports = prisma;
-// This file is used to create a Prisma client instance that can be used to interact with the database.
-// It imports the PrismaClient from the @prisma/client package and creates a new instance of it.
